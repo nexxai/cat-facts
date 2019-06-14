398235d0ad08e92336f44ffac68c281762cd9514
Justin Smith <nexxai@gmail.com>
2019-06-14 14:44:50 -0600

    Missed a variable

--- cat-facts-azure-logic-app.tf
+++ cat-facts-azure-logic-app.tf
@@ -2,6 +2,7 @@ variable organization_name = "Your organization's name"
 variable distribution_list_email_address = "catfacts@domain.com"
 variable your_name = "Macaulay Culkin"
 variable boss_name = "Joe Pesci"
+variable city = "New York"
 
 resource "azurerm_resource_group" "cat-facts-app" {
   name     = "cat-facts-app"
@@ -97,7 +98,7 @@ resource "azurerm_logic_app_action_custom" "cat-facts-send-email" {
     "description": "Deliver the cat fact",
     "inputs": {
         "body": {
-            "Body": "<p><span style=\"font-size: 14px\"><strong>Thank you for subscribing to Cat Facts, ${var.organization_name}'s most trusted resource of feline-related information in the Greater [CITY] Area!</strong></span><br>\n<br>\nDid you know:<br>\n@{variables('CatFact')}<br>\n<span style=\"font-size: 10px\">Source: </span><a href=\"http://catfact.ninja/fact\"><span style=\"font-size: 10px\">catfact.ninja/fact</span></a><br>\n<br>\nWant more Cat Facts? Just wait 5 minutes and you'll get another one!<br>\n<br>\nWant to not receive more Cat Facts? &nbsp;<strong>TOO BAD.</strong><br>\n<br>\n<span style=\"font-size: 10px\">This fact was obtained and submitted via the \"Cat-Facts-App\" Azure Logic App, meticulously designed and implemented by ${var.your_name}. &nbsp;If you have any questions, comments, concerns, or general complaints, please direct them to his boss ${var.boss_name} for immediate disposal.</span></p>",
+            "Body": "<p><span style=\"font-size: 14px\"><strong>Thank you for subscribing to Cat Facts, ${var.organization_name}'s most trusted resource of feline-related information in the Greater ${var.city} Area!</strong></span><br>\n<br>\nDid you know:<br>\n@{variables('CatFact')}<br>\n<span style=\"font-size: 10px\">Source: </span><a href=\"http://catfact.ninja/fact\"><span style=\"font-size: 10px\">catfact.ninja/fact</span></a><br>\n<br>\nWant more Cat Facts? Just wait 5 minutes and you'll get another one!<br>\n<br>\nWant to not receive more Cat Facts? &nbsp;<strong>TOO BAD.</strong><br>\n<br>\n<span style=\"font-size: 10px\">This fact was obtained and submitted via the \"Cat-Facts-App\" Azure Logic App, meticulously designed and implemented by ${var.your_name}. &nbsp;If you have any questions, comments, concerns, or general complaints, please direct them to his boss ${var.boss_name} for immediate disposal.</span></p>",
             "Subject": "[IMPORTANT] Cat Fact!",
             "To": "${var.distribution_list_email_address}"
         },
